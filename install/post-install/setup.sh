fe6c113293274838d917495e14661671154400a6
nnova-dev <loann.fages@gmail.com>
2026-04-27 21:27:41 +0200

    test: testing if connection without ly is working

--- install/post-install/setup.sh
+++ install/post-install/setup.sh
@@ -12,10 +12,10 @@ finalize_koda() {
         setup_koda_symlinks # On relance la fonction des liens pour être sûr
     fi
     # 2. Activation du service Ly (Display Manager)
-    if pacman -Qs ly > /dev/null; then
-        echo "   Activation du service Ly..."
-        sudo systemctl enable ly@tty2.service
-    fi
+    # if pacman -Qs ly > /dev/null; then
+    #     echo "   Activation du service Ly..."
+    #     sudo systemctl enable ly@tty2.service
+    # fi
     # 3. Changement du shell
     [ "$SHELL" != "/usr/bin/zsh" ] && sudo chsh -s /usr/bin/zsh "$USER"
     cp "$KODA_PATH/version" "$HOME/.config/koda-version"
